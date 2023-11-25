ffa233747419080f9bf1cdbeb0086eca5f3c0e18
christoseteoglou <itsmeglue@gmail.com>
2023-11-25 19:49:41 +0100

    Initial Commit

---
+++ SideMenuSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SideMenuSwift
+//
+//  Created by Christos Eteoglou on 2023-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SideMenuSwift/SideMenuSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftApp.swift
+//  SideMenuSwift
+//
+//  Created by Christos Eteoglou on 2023-11-25.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
